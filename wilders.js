a8eb71305aecf47b30acf97cf9ee0ddf6b389050
Cindie Jouvin <cindie.jouvin@gmail.com>
2019-10-02 11:05:03 +0200

    first commit

---
+++ wilders.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+const start = () => {
+    console.log(chalk.blue('Jessy'));
+    console.log(chalk.cyan('Pablo'));
+    console.log(chalk.magentaBright('Pascal'));
+    console.log(chalk.greenBright('Sophie'));
+}
+start()
